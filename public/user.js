9bb9ee3bfca5d08d672e2104495f02fa5578e088
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-10-09 17:37:35 +0530

    feat: login frontend

--- public/user.js
+++ public/user.js
@@ -32,16 +32,45 @@ form.addEventListener('submit',(event) => {
         
     })
     .catch(err => {
-        if (err.response && err.response.status === 409) {
+        if (err.response && err.response.status === 409) { //409 conflict status code(violates the unique constraint)
             alert('This email is already registered. Please use another one or log in.');
         } else {
             console.error(err);
         }
 });
 
-    
+})
+
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    if(!email || !password){
+        alert("Please fill email and password properly");
+        return;
+    }
 
 
+    const User ={ email:email,password:password};
+
+    axios.post(`${BASE_URL}/user/login`,{email,password})
+        .then(response => {
+            localStorage.setItem('authToken',response.data.token);
+            // window.location.href='dashboard.html';
+        })
+        .catch(error => {
+            if(error.response && error.response.status == 401){
+                alert("Incorrect Credentials");
+            }
+            else{
+                alert('Login failed. Please try again later.')
+            }
+        })
 
 })
 
